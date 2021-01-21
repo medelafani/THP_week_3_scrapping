a67374daba5943ce55cc8e847e3c85b8961ae1df
medelafani <76999928+medelafani@users.noreply.github.com>
2021-01-21 22:06:03 +0100

    Add files via upload

--- lib/app2_test.rb
+++ lib/app2_test.rb
@@ -1,8 +1,63 @@
-require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'rubygems'
+$principal_adress="http://annuaire-des-mairies.com"
 
-page = Nokogiri::HTML(URI.open("https://fr.wikipedia.org/wiki/France"))
-page.xpath('//h3/span').each do |cat|
-  puts cat.text
+
+def scrapper
+    return doc = Nokogiri::HTML(URI.open('http://annuaire-des-mairies.com/val-d-oise.html'))
+ end
+
+def city_mail
+    array_name = []
+    array_mail = []
+    page_city = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/44/nantes.html"))
+    mail_city = page_city.xpath('//html/body/div[1]/main/section[2]/div/table/tbody/tr[4]/td[2]')
+    name_city = page_city.xpath('//html/body/div[1]/main/section[1]/div/div/div/h1')
+
+    m_city = mail_city.text
+    n_city = name_city.text
+
+    mon_hash = Hash[n_city, m_city]
+
+    puts mon_hash
 end
+
+ def get_mail(array_final,array_link)
+   puts "It's take time"
+     array_link.each{|link|
+     pages=Nokogiri::HTML(URI.open($principal_adress+link))
+     mail = pages.xpath('//section[2]//tr[4]/td[2]').text
+     array_final << mail
+     }
+     return array_final
+ end
+ 
+ def fusiontab(tab1,tab2)
+     return Hash[tab1.zip(tab2)]
+ end
+ 
+ def mairie(page)
+     array_of_mail=[]
+     array_of_link=[]
+     array_of_name=[]
+
+     links=page.xpath('//tr//td//p[1]//a')
+
+     links.each{|link|
+         array_of_name<<link.text
+     }
+     array_of_name.delete("Plan du site")
+     links.each{|link|
+     array_of_link<<link['href']
+     }
+    array_of_link.delete("plan-du-site.html")
+     for i in 0..array_of_link.length-1
+         array_of_link[i][0]=array_of_link[i][0].delete(".")
+     end
+     get_mail(array_of_mail,array_of_link)
+     hash=fusiontab(array_of_name,array_of_mail)
+     return hash
+ end
+print city_mail()
+ print mairie(scrapper)
