1651ab58b0e47ade48723fbe300bd82fff653dbd
medelafani <76999928+medelafani@users.noreply.github.com>
2021-01-22 09:57:23 +0100

    Update crypto.rb

--- lib/crypto.rb
+++ lib/crypto.rb
@@ -3,34 +3,29 @@ require 'nokogiri'
 require 'open-uri'
 
 def scrapper
-  $page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
-    $crypto_name = $page.xpath('//tr//td[3]//div')
-    $crypto_value =  $page.xpath('//tr//td[5]//div//a')
+  page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
 end
 
-def making_hash(crypto_name, crypto_value)
+def making_hash(page)
   name_array = []
   value_array = []
 
-  $crypto_name.each do |name|
+  crypto_name = page.xpath('//tr//td[3]//div')
+  crypto_value =  page.xpath('//tr//td[5]//div//a')
+
+  crypto_name.each do |name|
     name_array << name.text
   end
 
-  $crypto_value.each do |value|
+  crypto_value.each do |value|
     value_array << value.text
   end
 
+
   crypto_wip = Hash[name_array.zip value_array]
   crypto_wip.each do |name , value|
-   puts $crypto_hash = {name => value.to_f}
+   puts crypto_hash = {name => value}
   end
-
-  return $crypto_hash
 end
 
-def perform(crypto_name, crypto_value)
-scrapper
-making_hash(crypto_name, crypto_value)
-end
-
-perform($crypto_name, $crypto_value)
+making_hash(scrapper)
